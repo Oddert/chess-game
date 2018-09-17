b45486db8ab53060a49e77c5798c6ad71163a441
Robyn <oddert@gmail.com>
2018-09-17 08:25:19 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express'),
+      app = express(),
+      bodyParser = require('body-parser'),
+      cookieParser = require('cookie-parser'),
+      path = require('path')
+
+require('dotenv').config()
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(cookieParser())
+
+app.use(express.static(path.join(__dirname + '/client/build')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/build/index.html'))
+})
+
+const PORT = process.env.PORT || 5000
+var server = app.listen(
+  PORT,
+  () => console.log(`Server initialised on PORT: ${PORT}...`)
+)
